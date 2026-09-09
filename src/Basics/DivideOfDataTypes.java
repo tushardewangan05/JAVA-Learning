376e3189acd87a84d9081878b07f1012a46ed79f
Tushar <tdewangan2006@gmail.com>
2026-09-09 23:55:33 +0530

    Add DivideOfDataTypes

---
+++ src/Basics/DivideOfDataTypes.java
@@ -0,0 +1,23 @@
+package Basics;
+
+public class DivideOfDataTypes {
+    public static void main(String[] args) {
+        //        double x=5/2;
+//        System.out.println(x);
+        //Output=2.0
+
+//        double x=5.0/2.0;
+//        System.out.println(x);
+//        Output=2.5package Basics;
+//
+//public class DivisionOfDataTypes {
+//    public static void
+
+//        double x=5.0/2;
+//        System.out.println(x);
+//        Output=2.5
+
+        double x=5/2.0;
+        System.out.println(x);
+    }
+}
