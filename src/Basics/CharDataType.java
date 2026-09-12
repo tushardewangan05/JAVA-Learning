96283a36f5f98d3cdcc9644f8e07f6a409027331
Tushar <tdewangan2006@gmail.com>
2026-09-12 20:41:57 +0530

    Add new program

--- src/Basics/CharDataType.java
+++ src/Basics/CharDataType.java
@@ -7,5 +7,17 @@ public class CharDataType {
 
         char eva = 's';
         System.out.println(eva);
+
+        //typecasting - ek data type se doosra data type conversion
+        char ch = 'A';
+        int x = ch; //impicit typecasting
+        System.out.println(x);
+
+        char piyush ='a';
+        int y = (int)piyush;   // explicit typecasting
+        System.out.println(y);
+
+        char harsh = '3';
+        System.out.println((int)harsh);
     }
 }
