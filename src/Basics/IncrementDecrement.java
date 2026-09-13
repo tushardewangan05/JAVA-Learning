45def1e098994fd4ffd59da525860231407d9d28
Tushar <tdewangan2006@gmail.com>
2026-09-13 22:04:56 +0530

    Add new program

--- src/Basics/IncrementDecrement.java
+++ src/Basics/IncrementDecrement.java
@@ -23,5 +23,9 @@ public class IncrementDecrement {
         System.out.println(x++);
         System.out.println(x);
         System.out.println(++x);
+
+//        int x = 10;
+        int y = x++; // y = 10
+        System.out.println(x+" "+y);
     }
 }
