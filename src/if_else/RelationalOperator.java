eccbda95f4efa42a7d609fa2c4a5922184d1741f
Tushar <tdewangan2006@gmail.com>
2026-09-14 23:08:37 +0530

    Add new program

--- src/if_else/RelationalOperator.java
+++ src/if_else/RelationalOperator.java
@@ -3,5 +3,8 @@ package if_else;
 public class RelationalOperator {
     public static void main(String[] args) {
         System.out.println(5>3);
+
+        boolean x = 5>3;
+        System.out.println(x);
     }
 }
