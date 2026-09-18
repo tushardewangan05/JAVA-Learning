53c9178a88f84eb7371e40e8bfb6d7f9d22e50de
Tushar <tdewangan2006@gmail.com>
2026-09-18 18:27:56 +0530

    Add new file

--- src/if_else/AbsoluteValue.java
+++ src/if_else/AbsoluteValue.java
@@ -1,4 +1,17 @@
 package if_else;
 
+import java.util.Scanner;
+
 public class AbsoluteValue {
+    public static void main(String[] args) {
+        Scanner sb = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sb.nextInt();
+//        if(n >= 0) System.out.println(n);
+//        else System.out.println(-n);
+
+//Without else
+        if (n < 0) n = -n;
+        System.out.println(n);
+    }
 }
