990a6e61c9f078028bbfc5a1779c6a96c26718e5
Tushar <tdewangan2006@gmail.com>
2026-09-19 15:21:24 +0530

    Adding new file

---
+++ src/if_else/FourDigitNumber.java
@@ -0,0 +1,14 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class FourDigitNumber {
+    public static void main(String[] args) {
+        Scanner sb = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sb.nextInt();
+
+        if(n>999 && n<10000) System.out.println("Four Digit Number");
+        else System.out.println("Not a four digit mumber");
+    }
+}
---
+++ src/if_else/NamingPrint.java
@@ -0,0 +1,16 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class NamingPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enetr Number: ");
+        int n = sc.nextInt();
+
+//        if(n % 5 == 0) System.out.println("Riya");
+//        if(n % 3 == 0) System.out.println("Ranu");
+//        if(n % 5 && 3) System.out.println("Apurva");
+//        if(n % 5||3 ==0) System.out.println("Isha");
+    }
+}
