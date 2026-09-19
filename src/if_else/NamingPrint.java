7788c9110b4a73bbd14006b80b3000b691391625
Tushar <tdewangan2006@gmail.com>
2026-09-19 15:56:30 +0530

    Add new program

--- src/if_else/NamingPrint.java
+++ src/if_else/NamingPrint.java
@@ -8,9 +8,9 @@ public class NamingPrint {
         System.out.print("Enetr Number: ");
         int n = sc.nextInt();
 
-//        if(n % 5 == 0) System.out.println("Riya");
-//        if(n % 3 == 0) System.out.println("Ranu");
-//        if(n % 5 && 3) System.out.println("Apurva");
-//        if(n % 5||3 ==0) System.out.println("Isha");
+        if(n % 5 ==0 && n % 3 == 0) System.out.println("Apurva");
+        else if(n % 5 ==0) System.out.println("Riya");
+        else if(n % 3 ==0) System.out.println("Banu");
+        else System.out.println("Isha");
     }
 }
