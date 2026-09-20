c728ec1a9d629fafe06fd50f059b07fd95d34b72
Tushar <tdewangan2006@gmail.com>
2026-09-20 15:07:36 +0530

    Add new program

---
+++ src/if_else/DivisibleByFiveOrThree.java
@@ -0,0 +1,14 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class DivisibleByFiveOrThree {
+    public static void main(String[] args) {
+        Scanner sb = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sb.nextInt();
+
+        if(n % 5 == 0 || n % 3 == 0) System.out.println("Divisible by 5 or 3");
+        else System.out.println("Not divisible by 5 or 3");
+    }
+}
