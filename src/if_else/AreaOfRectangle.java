2125816bc14ddadbacd14da31f3e72137a7e2ee1
Tushar <tdewangan2006@gmail.com>
2026-09-23 00:54:44 +0530

    Add new file

---
+++ src/if_else/AreaOfRectangle.java
@@ -0,0 +1,18 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner sb = new Scanner(System.in);
+        System.out.print("Enter Length: ");
+        int l = sb.nextInt();
+        System.out.print("Enter Breadth: ");
+        int b = sb.nextInt();
+
+        int area = l*b;
+        int perimeter = 2*(l+b);
+        if(area>perimeter) System.out.println("Area is Greater than perimeter");
+        else System.out.println("Not greater");
+    }
+}
--- src/if_else/NamingPrint.java
+++ src/if_else/NamingPrint.java
@@ -8,9 +8,13 @@ public class NamingPrint {
         System.out.print("Enetr Number: ");
         int n = sc.nextInt();
 
-        if(n % 5 ==0 && n % 3 == 0) System.out.println("Apurva");
-        else if(n % 5 ==0) System.out.println("Riya");
-        else if(n % 3 ==0) System.out.println("Banu");
-        else System.out.println("Isha");
+        if(n % 5 ==0 && n % 3 == 0)
+            System.out.println("Apurva");
+        else if(n % 5 ==0)
+            System.out.println("Riya");
+        else if(n % 3 ==0)
+            System.out.println("Banu");
+        else
+            System.out.println("Isha");
     }
 }
